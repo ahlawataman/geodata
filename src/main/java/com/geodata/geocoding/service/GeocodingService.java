cb7a16eae27ae4381134b32488d8c343e517ed57
Aman Ahlawat <ahlawataman3@gmail.com>
2025-03-04 13:03:22 +0530

    update .gitignore

--- src/main/java/com/geodata/geocoding/service/GeocodingService.java
+++ src/main/java/com/geodata/geocoding/service/GeocodingService.java
@@ -1,14 +1,10 @@
 package com.geodata.geocoding.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
